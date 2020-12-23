67b74e962d0fa346f843ac957afdce72f76b0a8d
dolphius21 <johnrudolphlee@gmail.com>
2020-12-23 18:14:30 +0800

    refactor js code to make it more concise.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
+// Variables
 const countHTML = document.getElementById('count');
 const btns = document.querySelectorAll('.btn');
 let count = 0;
 
-const checkCount = () => { 
+// Updates the background color of the site.
+const updateColor = () => { 
    (count < 0) ? document.body.style.backgroundColor = '#de6868'
    : (count > 0) ? document.body.style.backgroundColor = '#68c49f'
    : document.body.style.backgroundColor = '#79d1ed';
 };
 
-const updateCount = () => countHTML.textContent = count;
-
+// Selects button and defines it's functionality.
 btns.forEach(btn => {
    btn.addEventListener('click', () => {
       (btn.title === 'increase') ? count++ 
       : (btn.title === 'decrease') ? count--
       : count = 0;
 
-      checkCount();
-      updateCount();
+      countHTML.textContent = count;
+      updateColor();
    })
 });
